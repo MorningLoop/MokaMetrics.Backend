6349782c21e2e5117fae84b2ccd53f283f6aa254
Raffaele <raffaele.trivillin@gmail.com>
2025-06-10 20:07:18 +0200

    feat(db context): add basic db context implementation

---
+++ src/MokaMetrics.DataAccess/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MokaMetrics.DataAccess.Abstractions.Contexts;
+using MokaMetrics.Models;
+
+namespace MokaMetrics.DataAccess.Contexts;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+    {
+        // cycling all entities flagged for creation/update and automatically setting updatedAt timestamp
+        foreach (var item in ChangeTracker.Entries<Entity>().AsEnumerable())
+            item.Entity.UpdatedAt = DateTime.UtcNow;
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
