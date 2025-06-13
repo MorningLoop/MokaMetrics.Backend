14a93853a593138b0573ae140a8b465d386d708e
Raffaele <raffaele.trivillin@gmail.com>
2025-06-13 14:46:42 +0200

    feat(influx): add models

---
+++ src/MokaMetrics.Models/Influx/QueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MokaMetrics.Models.Influx;
+
+public class QueryResult
+{
+    public string Measurement { get; set; } = string.Empty;
+    public Dictionary<string, object> Tags { get; set; } = new();
+    public Dictionary<string, object> Fields { get; set; } = new();
+    public DateTime Timestamp { get; set; }
+}
+
