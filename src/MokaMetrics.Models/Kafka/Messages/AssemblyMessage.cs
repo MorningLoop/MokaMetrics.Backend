58fc314e8bd360a257d09ea6cdb62d8c92360de8
Raffaele <raffaele.trivillin@gmail.com>
2025-06-25 22:14:55 +0200

    feat(kafka): add machine messages templates

---
+++ src/MokaMetrics.Models/Kafka/Messages/AssemblyMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace MokaMetrics.Models.Kafka.Messages;
+
+public class AssemblyMessage : MachineKafkaMessage
+{
+    public float AverageTimePerStation { get; set; }
+    public int ActiveOperators { get; set; }
+}
---
+++ src/MokaMetrics.Models/Kafka/Messages/MachineKafkaMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace MokaMetrics.Models.Kafka.Messages;
+
+public class MachineKafkaMessage
+{
+    public string Site { get; set; } // italy, brazil, vietnam
+    public string LotCode { get; set; }
+    public DateTime LocalTimestamp { get; set; }
+    public DateTime UtcTimestamp { get; set; }
+    public string MachineId { get; set; }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
