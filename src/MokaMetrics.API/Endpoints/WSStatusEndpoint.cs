faaa84aa5690045933548da125a1aec58a21c2a9
Raffaele <raffaele.trivillin@gmail.com>
2025-06-26 10:39:47 +0200

    feat: add kafka consumer worker project

--- src/MokaMetrics.API/Endpoints/WSStatusEndpoint.cs
+++ src/MokaMetrics.API/Endpoints/WSStatusEndpoint.cs
@@ -21,85 +21,85 @@ namespace MokaMetrics.API.Endpoints
         }
 
 
-        //private static async Task<Results<Ok, BadRequest<string>>> WSStatusMachine(HttpContext _httpContext, IKafkaProducer _kafkaProducer)
-        //{
-        //    if (!_httpContext.WebSockets.IsWebSocketRequest)
-        //    {
-        //        return TypedResults.BadRequest("This connection is not a WebSocket!");
-        //    }
+        private static async Task<Results<Ok, BadRequest<string>>> WSStatusMachine(HttpContext _httpContext, IKafkaProducer _kafkaProducer)
+        {
+            if (!_httpContext.WebSockets.IsWebSocketRequest)
+            {
+                return TypedResults.BadRequest("This connection is not a WebSocket!");
+            }
 
-        //    try
-        //    {
-        //        var webSocket = await _httpContext.WebSockets.AcceptWebSocketAsync();
-        //        var cancellationToken = _httpContext.RequestAborted;
+            try
+            {
+                var webSocket = await _httpContext.WebSockets.AcceptWebSocketAsync();
+                var cancellationToken = _httpContext.RequestAborted;
 
-        //        Console.WriteLine("WebSocket connesso");
+                Console.WriteLine("WebSocket connesso");
 
-        //        // Invia immediatamente l'ultimo messaggio disponibile
-        //        try
-        //        {
-        //            string? lastMessage = await _kafkaProducer.GetLastAvailableMessageAsync();
-        //            if (!string.IsNullOrEmpty(lastMessage))
-        //            {
-        //                Console.WriteLine($"Invio ultimo messaggio disponibile: {lastMessage}");
-        //                var bytes = Encoding.UTF8.GetBytes(lastMessage);
-        //                await webSocket.SendAsync(
-        //                    bytes,
-        //                    WebSocketMessageType.Text,
-        //                    endOfMessage: true,
-        //                    cancellationToken
-        //                );
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine($"Errore nell'invio dell'ultimo messaggio: {ex.Message}");
-        //        }
-
-                
-                while (webSocket.State == WebSocketState.Open && !cancellationToken.IsCancellationRequested)
+                // Invia immediatamente l'ultimo messaggio disponibile
+                try
                 {
-                    try
-                    {
-                        
-                        string? message = await _kafkaService.GetLatestMessageAsync(cancellationToken);
-                        
-                        if (!string.IsNullOrEmpty(message))
-                        {
-                            Console.WriteLine($"Nuovo messaggio ricevuto da Kafka: {message}");
-                            
-                            var bytes = Encoding.UTF8.GetBytes(message);
-                            await webSocket.SendAsync(
-                                bytes,
-                                WebSocketMessageType.Text,
-                                endOfMessage: true,
-                                cancellationToken
-                            );
-                        }
-                        
-                        await Task.Delay(100, cancellationToken);
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        Console.WriteLine("WebSocket operazione cancellata");
-                        break;
-                    }
-                    catch (WebSocketException wsEx)
-                    {
-                        Console.WriteLine($"WebSocket errore: {wsEx.Message}");
-                        break;
-                    }
+                    //string? lastMessage = await _kafkaProducer.GetLastAvailableMessageAsync();
+                    //if (!string.IsNullOrEmpty(lastMessage))
+                    //{
+                    //    Console.WriteLine($"Invio ultimo messaggio disponibile: {lastMessage}");
+                    //    var bytes = Encoding.UTF8.GetBytes(lastMessage);
+                    //    await webSocket.SendAsync(
+                    //        bytes,
+                    //        WebSocketMessageType.Text,
+                    //        endOfMessage: true,
+                    //        cancellationToken
+                    //    );
+                    //}
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Errore nell'invio dell'ultimo messaggio: {ex.Message}");
                 }
 
-        //        // Chiudi la connessione WebSocket se non è già chiusa
-        //        if (webSocket.State == WebSocketState.Open)
-        //        {
-        //            await webSocket.CloseAsync(
-        //                WebSocketCloseStatus.NormalClosure,
-        //                "Connessione chiusa",
-        //                CancellationToken.None
-        //            );
-        //        }
+
+                while (webSocket.State == WebSocketState.Open && !cancellationToken.IsCancellationRequested)
+                {
+                    //try
+                    //{
+
+                    //    string? message = await _kafkaProducer.GetLatestMessageAsync(cancellationToken);
+
+                    //    if (!string.IsNullOrEmpty(message))
+                    //    {
+                    //        Console.WriteLine($"Nuovo messaggio ricevuto da Kafka: {message}");
+
+                    //        var bytes = Encoding.UTF8.GetBytes(message);
+                    //        await webSocket.SendAsync(
+                    //            bytes,
+                    //            WebSocketMessageType.Text,
+                    //            endOfMessage: true,
+                    //            cancellationToken
+                    //        );
+                    //    }
+
+                    //    await Task.Delay(100, cancellationToken);
+                    //}
+                    //catch (OperationCanceledException)
+                    //{
+                    //    Console.WriteLine("WebSocket operazione cancellata");
+                    //    break;
+                    //}
+                    //catch (WebSocketException wsEx)
+                    //{
+                    //    Console.WriteLine($"WebSocket errore: {wsEx.Message}");
+                    //    break;
+                    //}
+                }
+
+                // Chiudi la connessione WebSocket se non è già chiusa
+                if (webSocket.State == WebSocketState.Open)
+                {
+                    await webSocket.CloseAsync(
+                        WebSocketCloseStatus.NormalClosure,
+                        "Connessione chiusa",
+                        CancellationToken.None
+                    );
+                }
 
                 return TypedResults.Ok();
             }
@@ -110,6 +110,6 @@ namespace MokaMetrics.API.Endpoints
             }
         }
 
-        
+
     }
 }
