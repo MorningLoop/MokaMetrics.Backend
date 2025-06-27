ee55e1cecaff6ad9d89f9c0b9540268c699c3643
Raffaele <raffaele.trivillin@gmail.com>
2025-06-27 19:31:40 +0200

    feat(DB): add machine code to machine model

--- src/MokaMetrics.Models/Entities/Machine.cs
+++ src/MokaMetrics.Models/Entities/Machine.cs
@@ -2,6 +2,7 @@
 
 public class Machine : Entity
 {
+    public string Code { get; set; }
     public string Model { get; set; }
     public int Status { get; set; }
     public int IndustrialFacilityId { get; set; }
